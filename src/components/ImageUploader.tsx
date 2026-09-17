f3491f924ceb7d42e4c7406b6d1b1a84362ed31d
shotaro0222 <98463339+shotaro0222@users.noreply.github.com>
2026-09-17 16:50:04 +0900

    Update ImageUploader.tsx

--- src/components/ImageUploader.tsx
+++ src/components/ImageUploader.tsx
@@ -23,7 +23,7 @@ export default function ImageUploader() {
 
     try {
       // ※ご自身のXserverのドメイン（アップロード先）に変更してください
-      const res = await fetch('https://conconsalsal.com/upload-api.php', {
+      const res = await fetch('https://conconsalsal.com/public_html/upload-api.php', {
         method: 'POST',
         body: formData,
       });
